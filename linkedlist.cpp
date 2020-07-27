43c79315771fe36f3c542f2ab0566c7d66a0d2d0
YashThakare <yashthakare9498@gmail.com>
2020-07-27 21:38:16 +0530

    Update linkedlist.cpp
    All operations are added to implement in Linked List in C++

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 struct node
 {
-    /* data */
     int data;
     node *link;
 };
@@ -29,7 +28,6 @@ public:
 
         if (head == NULL)
         {
-            /* code */
             head = temp;
             tail = temp;
         }
@@ -40,17 +38,56 @@ public:
         }
     }
 
-    void show()
+    void concatenate(node *a,node *b)
+    {
+        if( a != NULL && b!= NULL )
+        {
+            if (a->next == NULL)
+                a->next = b;
+            else
+                concatenate(a->next,b);
+        }
+        else
+        {
+            cout << "Either a or b is NULL\n";
+        }
+    }
+
+    void front(int n)
+    {
+        node *tmp = new node;
+        tmp -> data = n;
+        tmp -> next = head;
+        head = tmp;
+    }
+
+    void after(node *a, int value)
+    {
+        node* p = new node;
+        p->data = value;
+        p->next = a->next;
+        a->next = p;
+    }
+
+    void delete1 (node *before_del)
+    {
+        node* temp;
+        temp = before_del->next;
+        before_del->next = temp->next;
+        delete temp;
+    }
+
+    void display()
     {
         node *p = head;
 
         while (p != NULL)
         {
-            cout << "\n"
-                 << p->data << "\n";
+            cout << p->data <<endl;;
 
             p = p->link;
         }
+        cout<<endl;
     }
 };
 
@@ -59,6 +96,19 @@ int main(int argc, char const *argv[])
     linked_list a;
     a.add_node(1);
     a.add_node(2);
-    
-    a.show();
+    a.add_node(3);
+    a.add_node(4);
+    a.add_node(5);
+    a.add_node(6);
+    a.display();
+    linked_list b;
+    b.add_node(9);
+    b.add_node(10);
+    a.concatenate(a.gethead(),b.gethead());
+    a.display();
+    a.front(20);
+    a.after(30);
+    a.display();
+    a.delete1(a.gethead()->next);
+    a.display();
 }
